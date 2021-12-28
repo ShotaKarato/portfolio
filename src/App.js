6a8b127d718c9b6300d8a1242ed1c2002b2fc63a
ShotaKarato <theseuta@gmail.com>
2021-12-28 21:45:59 +0900

    feat: footer component added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Career from "./components/Career";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Career />
       <Projects />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>Copyright © 2021 Shota Karato - built with React.js</small>
+    </footer>
+  );
+}
