8908f6d15cfe8f42db75004c2a0dfa53eb2e5e4b
Shota Karato <theseuta@gmail.com>
2023-04-08 18:02:10 +0900

    :bug: replace createTheme with createGlobalTheme
    replace createTheme with createGlobalTheme

--- src/styles/theme.css.ts
+++ src/styles/theme.css.ts
@@ -1,11 +1,17 @@
-import { createTheme } from "@vanilla-extract/css";
+import { createGlobalTheme } from "@vanilla-extract/css";
 
-export const [themeClass, vars] = createTheme({
-  fontSize: {
-    small: "14px",
-    base: "16px",
-    medium: "24px",
-    large: "36px",
+export const vars = createGlobalTheme(":root", {
+  font: {
+    size: {
+      small: "14px",
+      base: "16px",
+      medium: "24px",
+      large: "36px",
+    },
+    weight: {
+      medium: "500",
+      bold: "700",
+    },
   },
   color: {
     primary: "#ded7c6",
