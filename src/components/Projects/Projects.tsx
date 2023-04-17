796b17523da44d3a3a3855077f163f99a6a9165e
Shota Karato <theseuta@gmail.com>
2023-04-17 21:34:23 +0900

    :hammer: create ProjectsContainer
    create ProjectsContainer

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,15 +2,16 @@ import { Overlay, OverlayProps } from "~/src/ui/Overlay";
 import { Heading } from "~/src/ui/Heading";
 import { ProjectItem } from "./ProjectItem";
 import type { Props as ProjectItemProps } from "./ProjectItem";
-import { projects } from "./mock";
 import { projectsStyles } from "./Projects.css";
 
 export type Props = {
+  readonly projects: Omit<ProjectItemProps, "onOverlayOpen">[];
   readonly isOverlayOpen: OverlayProps["isOpen"];
   readonly onOverlayOpen: ProjectItemProps["onOverlayOpen"];
   readonly onOverlayClose: OverlayProps["onClose"];
 };
 export const Projects = ({
+  projects,
   onOverlayOpen,
   isOverlayOpen,
   onOverlayClose,
--- src/components/Projects/ProjectsContainer.tsx
+++ src/components/Projects/ProjectsContainer.tsx
@@ -1,10 +1,12 @@
 import { useOverlay } from "~/src/ui/Overlay/utils/useOverlay";
 import { Projects } from "./Projects";
+import { projects } from "~/src/data/projects";
 
 export const ProjectsContainer = () => {
   const { isOverlayOpen, handleOverlayOpen, handleOverlayClose } = useOverlay();
   return (
     <Projects
+      projects={projects}
       isOverlayOpen={isOverlayOpen}
       onOverlayOpen={handleOverlayOpen}
       onOverlayClose={handleOverlayClose}
