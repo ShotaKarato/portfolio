2969af5a3155ef89e96840c213f17cdb1ad73674
Shota Karato <theseuta@gmail.com>
2023-04-19 22:05:19 +0900

    :wrench: add d.ts file to get type suggestion working
    add d.ts file to get type suggestion working

---
+++ types/vite-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  NOTION_API_KEY: string;
+  NOTION_BIO_DB_ID: string;
+  NOTION_CAREER_DB_ID: string;
+  NOTION_PROJECTS_DB_ID: string;
+}
