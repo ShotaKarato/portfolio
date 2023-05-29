1e873c81ae04061ff12b48519cc9c9463688b782
Shota Karato <theseuta@gmail.com>
2023-05-29 22:41:55 +0900

    :adhesive_bandage: update d.ts file for environment variables
    update d.ts file for environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  VITE_NOTION_API_KEY: string;
+  VITE_NOTION_BIO_DB_ID: string;
+  VITE_NOTION_CAREER_DB_ID: string;
+  VITE_NOTION_PROJECTS_DB_ID: string;
+  VITE_PORT: number;
+}
