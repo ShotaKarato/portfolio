8b6fc8d917791e4bbebeb24a41db1957d53637ea
Shota Karato <theseuta@gmail.com>
2023-06-07 22:31:28 +0900

    :hammer: pass projects data to client
    pass projects data to client

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Bio } from "./components/Bio";
 import { Outline } from "./components/Outline";
-import { Projects } from "./components/Projects";
+import { Projects, ProjectsProps } from "./components/Projects";
 
 import "./styles/app.css";
 import type { BioProps } from "./components/Bio";
 
-type Props = BioProps;
-export const App = (props: Props) => (
+type Props = ProjectsProps & BioProps;
+export const App = ({ projects, ...rest }: Props) => (
   <div className="App">
     <Outline>
-      <Bio {...props} />
-      <Projects />
+      <Bio {...rest} />
+      <Projects projects={projects} />
     </Outline>
   </div>
 );
--- src/components/Projects/ProjectsContainer.tsx
+++ src/components/Projects/ProjectsContainer.tsx
@@ -1,9 +1,14 @@
 import { useOverlay } from "~/src/ui/Overlay/utils/useOverlay";
 import { Projects } from "./Projects";
-import { projects } from "~/src/data/projects";
+// import { projects } from "~/src/data/projects";
 import { useSelectedProject } from "./utils/useSelectedProject";
+import { Project } from "./types";
 
-export const ProjectsContainer = () => {
+export type Props = {
+  readonly projects: Project[];
+};
+
+export const ProjectsContainer = ({ projects }: Props) => {
   const { isOverlayOpen, handleOverlayOpen, handleOverlayClose } = useOverlay();
   const { selectedProject, handleProjectSelect } = useSelectedProject(projects);
 
